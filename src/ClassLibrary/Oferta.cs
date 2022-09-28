41f28411bd5057bc96cc44354893b07d52926d49
Maik <maicol.marcenaro@correo.ucu.edu.uy>
2022-09-28 20:52:36 -0300

    atributo_precio

--- src/ClassLibrary/Oferta.cs
+++ src/ClassLibrary/Oferta.cs
@@ -4,9 +4,10 @@ using System.Collections.Generic;
 
 public class Oferta   
 {
-    public Oferta(string categoria, ArrayList<int> coordenadas)
+    public Oferta(string categoria, ArrayList<int> coordenadas, decimal precio)
     {
         this.categoria = categoria;
         this.coordenadas = coordenadas;
+        this.precio= precio;
     }
 }
