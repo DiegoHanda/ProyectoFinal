42119af19547e9d4f00dc7bed5716e02f2fcadb9
DiegoHanda <diego.handalian@correo.ucu.edu.uy>
2022-09-28 16:14:46 -0300

    Reposotorio con Consigna

--- src/Program/Program.cs
+++ src/Program/Program.cs
@@ -5,6 +5,7 @@
 //--------------------------------------------------------------------------------
 
 using System;
+using System.Collections.Generic;
 using ClassLibrary;
 
 namespace ConsoleApplication
@@ -19,9 +20,6 @@ namespace ConsoleApplication
         /// </summary>
         public static void Main()
         {
-            var train = new Train();
-            train.StartEngines();
-            Console.WriteLine("Hello World!");
         }
     }
 }
