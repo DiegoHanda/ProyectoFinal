a256b77cee71275778ac67ea7d88356d19ebd631
DiegoHanda <diego.handalian@correo.ucu.edu.uy>
2022-09-28 18:59:04 -0300

    Clase Servicio

---
+++ src/ClassLibrary/Categoria.cs
@@ -0,0 +1,16 @@
+public class Categoria
+{
+    public Categoria()
+    {
+
+    }
+    public list<T> categorizar(string CATEGORIA)
+    {/*
+        -llamo a lista con todos las ofertas
+        -lista vacia
+        -recorro la primer lista 
+        -por cada elemento hago get.categoria
+        -si get.categoria==CATEGORIA, lo agrego
+        -retornar lista vacia con todas las ofertas que tengan esta categoria*/
+    }
+}
--- src/ClassLibrary/Servicio.cs
+++ src/ClassLibrary/Servicio.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using ClassLibrary;
 
 namespace ClassLibrary;
 public class Servicio
