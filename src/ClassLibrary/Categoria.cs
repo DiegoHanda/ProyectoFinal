2318005768c3e5d14df59db8b920080f010b774c
Maik <maicol.marcenaro@correo.ucu.edu.uy>
2022-09-28 22:46:48 -0300

    ClassesCategoriasYChat

--- src/ClassLibrary/Categoria.cs
+++
@@ -1,8 +0,0 @@
-using System;
-namespace ClassLibrary;
-
-public class Categoria //Esto va aca?
-{
-    List<string> categorias = new List<string>();
- 
-}
---
+++ src/ClassLibrary/Categorias.cs
@@ -0,0 +1,8 @@
+using System;
+namespace ClassLibrary;
+
+public class Categorias//Esto va aca?
+{
+    public string nombre= nombre;
+
+}
