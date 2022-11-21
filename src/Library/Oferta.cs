7507c667e9cc52511b7dd9df1ccd0baab04be25a
braimor <briangmsosa@gmail.com>
2022-11-20 23:57:58 -0300

    Add PorReputacion and PorCategoria methods to OfertasManager

--- src/Library/Oferta.cs
+++ src/Library/Oferta.cs
@@ -5,10 +5,10 @@ using System.Collections.Generic;
 public class Oferta   
 {
     public Categoria Categoria {get; set;}
-    public Usuario Ofertante {get; set;}
+    public Trabajador Ofertante {get; set;}
     public string Descripcion {get; set;}
 
-    public Oferta(Categoria categoria,  Usuario ofertante, string descripcion)
+    public Oferta(Categoria categoria,  Trabajador ofertante, string descripcion)
     {
         this.Categoria = categoria;
         this.Ofertante = ofertante;
