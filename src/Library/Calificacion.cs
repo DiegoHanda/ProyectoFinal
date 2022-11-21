8b5ad053bf9392ea650a7486bf47f68f03101c4f
DiegoHanda <diego.handalian@correo.ucu.edu.uy>
2022-11-21 13:40:43 -0300

    Calificacion´s value Validation

--- src/Library/Calificacion.cs
+++ src/Library/Calificacion.cs
@@ -9,7 +9,19 @@ public class Calificacion
     public Cliente Calificado {get; set;}
     public Calificacion(int value, Cliente calificador, Cliente calificado)
     {
-        this.Value = value;
+        if (value == null)
+        {
+            throw new Exception ("No se ha ingresado ningún número");
+        }
+        else if (value<1 || value>5)
+        {
+            throw new Exception ($"{value} no es un puntaje válido");
+        }
+        else
+        {
+            this.Value = value;
+        }
+        
         this.Calificador = calificador;
         this.Calificado = calificado;
     }
