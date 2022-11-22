d0e337599c8252d39ee7982db5e9bc10e6bdb73e
Javier Odera <javier.odera@correo.ucu.edu.uy>
2022-11-22 14:19:03 -0300

    Retoques comentados

--- src/Library/Calificacion.cs
+++ src/Library/Calificacion.cs
@@ -4,7 +4,7 @@ using System.Collections.Generic;
 
 public class Calificacion   
 {
-    public int Value {get; set;}    //Algunas de estas clases fueron separadas desde conceptos mas abstractos. Creando clases que tengan resposabilidades especificas
+    public int Value {get; set;}    //Algunas de estas clases (Calificacion,Contrato, Ofertas)fueron separadas desde conceptos mas abstractos. Creando clases que tengan resposabilidades especificas
                                     // para cumplir con el SRP
     public Cliente Calificador {get; set;}
     public Cliente Calificado {get; set;}
