190d13e10a0ebe228b5eeb5094b3cb3ea1a7ba7c
Javier Odera <javier.odera@correo.ucu.edu.uy>
2022-11-22 14:35:42 -0300

    mas cambios

--- src/Library/CategoriasManager.cs
+++ src/Library/CategoriasManager.cs
@@ -8,8 +8,7 @@ public sealed class CategoriasManager
     private readonly static CategoriasManager _instance = new CategoriasManager(); // Aca se usa un singleton para que esta clase solo pueda ser instanciada una vez
     private CategoriasManager() {}
                                         //Intentamos mantener un bajo acoplamiento de las clases al evitar todo lo posible que esten relacionadas
-                                        //La mayoria de nuestras clases no dependen de otras
-                                        //Intentamos mantener la coherencia dentro de las clases sin aumentar su acoplamiento
+                                        //Intentamos mantener la coherencia dentro de las clases sin aumentar su acoplamiento. 
     public static CategoriasManager Instance { get { return _instance; } }
     public List<Categoria> Categorias = new List<Categoria>();
     public void CrearCategoria(string nombre) //Este método crea una instancia de Categoria
